de15c00161dd1cfdc06e5f935baa0cb1a2a0e8e3
Daniel Campman <dwc1280@rit.edu>
2022-02-25 17:42:16 -0500

    Bug Fix
    - This time I *actually* fixed the bot
    - Removed unnecessary debug print statements

--- embedGenerator/__init__.py
+++ embedGenerator/__init__.py
@@ -12,6 +12,4 @@ async def process_message(message: Message):
     :param message: The message to parse
     """
     for subclass in BaseGenerator.__subclasses__():
-        print(f"Calling {subclass.__name__}")
         await subclass.run(message)
-    print("Processed")
